aa379d148e664e77ad8432a9893a52eb937bdc4a
zanzydnd <zanzydnd@gmail.com>
2020-09-17 09:45:03 +0300

    Create V1_create_user.sql

---
+++ V1_create_user.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `db1`.`user1` (
+  `username` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `password` VARCHAR(32) NOT NULL,
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP);
